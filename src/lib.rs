c5a1726cd32395c22c0e017c38d4651847e3c04d
rasviitanen <rasviitanen@gmail.com>
2018-03-10 13:30:49 +0100

    svg test file

--- src/lib.rs
+++ src/lib.rs
@@ -84,15 +84,6 @@ macro_rules! svg {
 
 #[cfg(test)]
 mod tests {
-
-    fn save(data: String) {
-        use std::fs::File;
-        use std::io::Write;
-
-        let mut f = File::create("foo.svg").expect("Unable to create file");
-        f.write_all(data.as_bytes()).expect("Unable to write data");
-    
-    }
     
     #[test]    
     fn test_flat() {
@@ -186,25 +177,6 @@ mod tests {
         assert_eq!(out, "<svg width=\"200\"><circle width=\"200\"/>200</svg>");
     }
 
-    #[test]
-    fn save_svg() {
-        fn content() -> String {
-            use std::fmt::Write;
-            let mut out = String::new();
-            svg!(&mut out,
-                svg() [
-                    g() ["Hello"]            
-                ]
-            );
-            out
-        }
-        use std::fs::File;
-        use std::io::Write;
-        let data = "Some data!";
-        let mut f = File::create("test.svg").expect("Unable to create file");
-        f.write_all(content().as_bytes()).expect("Unable to write data");
-        assert_eq!(content(), "<svg><g>Hello</g></svg>");
-    }
     #[test]    
     fn test_str_as_variable() {
         let my_str: &str = "200";
@@ -216,37 +188,6 @@ mod tests {
             ]
         );
         assert_eq!(out, "<svg width=\"200\"><circle width=\"200\"/></svg>");
-    }
-    
-    #[test]    
-    fn test_loop() {
-        use std::fmt::Write;
-        let mut out = String::new();
-        svg!(&mut out,
-            svg(version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px" 
-                y="0px"
-                width="500px" 
-                height="200px" 
-                viewBox="0 0 50 10"
-                xml:space="preserve"
-                ) 
-            [
-                @ for i in 0..5 {
-                    svg!(&mut out,
-                        rect(x={i*10} y="10" width="4" height="10" fill="#333" opacity="0.2") [
-                        animate(attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite")                        
-                        animate(attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite")
-                        animate(attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite")
-                    ]);
-                };
-            ]
-        );
-        save(out);
-        //assert_eq!(out, "<svg><circle width=\"100\"/><rect width=\"200\"/></svg>");
     }
 
 }
